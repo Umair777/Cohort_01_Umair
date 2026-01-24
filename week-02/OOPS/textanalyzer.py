9f9c5c0506a6a89424cd28971bfd3e9c939fd255
Umair Ashraf <36963276+Umair777@users.noreply.github.com>
2026-01-24 20:25:24 +0530

    updated text analyzer

--- week-02/OOPS/textanalyzer.py
+++ week-02/OOPS/textanalyzer.py
@@ -11,6 +11,22 @@ class TextAnalyzer(object):
         formatted_text = text.replace('.',"").replace(',',"").replace('!',"").replace('?',"")
         formatted_text = formatted_text.lower()
         self.fmtText = formatted_text
+    
+    #count the frequency of all unique words
+    
+    def word_frequency(self):
+        words = self.fmtText.split()
+        freqMap = {}
+        for word in set(words):
+            freqMap[word] = words.count(word)
+        return freqMap
+        # frequency = {}
+        # for word in words:
+        #     if word in frequency:
+        #         frequency[word] += 1
+        #     else:
+        #         frequency[word] = 1
+        # return frequency
         
 if __name__ == "__main__":
     print('Text before formatting:')
@@ -19,6 +35,13 @@ if __name__ == "__main__":
     analyzer = TextAnalyzer(sample_text)
     print("Text after formatting:")
     print(analyzer.fmtText)
+    print("Word Frequency:")
+    freq = analyzer.word_frequency()
+    print(freq)
+    print('datatype of the frequency: ',type(freq))
+    # for word, count in freq.items():
+    #     print(f"'{word}': {count}") 
+    
 
 '''OUTPUT:Text before formatting:
 Hello there! Welcome to the world of Python programming. 
@@ -29,4 +52,9 @@ Text Analyzer Initialized
 Text after formatting:
 hello there welcome to the world of python programming 
 this text analyzer will help you understand the frequency of words in a given text 
-let's analyze this text'''
+let's analyze this text
+
+Word Frequency:
+{'understand': 1, 'hello': 1, 'help': 1, 'text': 3, 'words': 1, 'a': 1, "let's": 1, 'analyzer': 1, 'there': 1, 'of': 2, 'given': 1, 'programming': 1, 'you': 1, 'world': 1, 'will': 1, 'analyze': 1, 'in': 1, 'the': 2, 'frequency': 1, 'python': 1, 'welcome': 1, 'to': 1, 'this': 2}
+datatype of the frequency:  <class 'dict'>'''
+
